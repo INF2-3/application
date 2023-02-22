1143cc3df03ebdb1b34941ffd09d6ef6c3380701
Max Appeldorn <mappeldorn1@st.noorderpoort.nl>
2023-02-22 12:28:03 +0100

    added navbar

---
+++ Quintor/src/main/java/com/example/quintor/controller/NavbarController.java
@@ -0,0 +1,41 @@
+package com.example.quintor.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class NavbarController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    public void initialize(){
+//        loadPage("dashboard");
+    }
+
+    public void dashboardButton(){
+//        loadPage("dashboard");
+    }
+
+    public void transactionButton(){
+//        loadPage("transactions");
+    }
+
+    public void settingsButton(){
+//        loadPage("settings");
+    }
+
+    public void loadPage(String page) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(page + ".fxml")));
+        }catch (IOException ex){
+            System.out.println(ex);
+        }
+        borderPane.setCenter(root);
+    }
+}
