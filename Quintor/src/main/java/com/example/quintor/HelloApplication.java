8c8a837b293e5094dfb787839824e6f21f890877
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2023-02-22 12:34:24 +0100

    made layout for transactions overview and started styling table

--- Quintor/src/main/java/com/example/quintor/HelloApplication.java
+++ Quintor/src/main/java/com/example/quintor/HelloApplication.java
@@ -2,17 +2,43 @@ package com.example.quintor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    int screenWidth = (int) Screen.getPrimary().getBounds().getWidth();
+    int screenHeight = (int) Screen.getPrimary().getBounds().getHeight();
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
+        // Responsive Design
+        int sceneWidth = 0;
+        int sceneHeight = 0;
+        if (screenWidth <= 800 && screenHeight <= 600) {
+            sceneWidth = 600;
+            sceneHeight = 350;
+        } else if (screenWidth <= 1280 && screenHeight <= 768) {
+            sceneWidth = 800;
+            sceneHeight = 450;
+        } else if (screenWidth <= 1920 && screenHeight <= 1080) {
+            sceneWidth = 1000;
+            sceneHeight = 650;
+        }
+
+        // Scene
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), sceneWidth, sceneHeight);
+        String css = this.getClass().getResource("/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
---
+++ Quintor/src/main/java/com/example/quintor/TransactionOverviewController.java
@@ -0,0 +1,37 @@
+package com.example.quintor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TransactionOverviewController {
+
+    @FXML
+    private TableColumn<?, ?> columnBedrag;
+
+    @FXML
+    private TableColumn<?, ?> columnCategory;
+
+    @FXML
+    private TableColumn<?, ?> columnCode;
+
+    @FXML
+    private TableColumn<?, ?> columnDate;
+
+    @FXML
+    private TableColumn<?, ?> columnDebCred;
+
+    @FXML
+    private TableColumn<?, ?> columnDescription;
+
+    @FXML
+    private TableColumn<?, ?> columnType;
+
+    @FXML
+    private TableView<?> transactionsTable;
+
+    public void test() {
+
+    }
+}
+
