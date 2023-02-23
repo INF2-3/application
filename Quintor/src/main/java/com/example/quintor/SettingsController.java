f08ef0a03cc24a41b6fcae51ef97d5d9d09082d7
Max Appeldorn <mappeldorn1@st.noorderpoort.nl>
2023-02-23 14:42:21 +0100

    change view

---
+++ Quintor/src/main/java/com/example/quintor/SettingsController.java
@@ -0,0 +1,53 @@
+package com.example.quintor;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class SettingsController {
+    @FXML
+    private AnchorPane anchorPane;
+    public VBox settingsBox;
+
+    public void changePassword(){
+//        changeView("changePassword");
+    }
+
+    public void userOverview() {
+//        changeView("userOverview");
+    }
+
+    public void addonOverview(){
+        changeView("addonOverview");
+    }
+
+    public void logout() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("navbar.fxml"));
+        Parent root = loader.load();
+        Scene newScene = new Scene(root);
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        Scene oldScene = stage.getScene();
+        if (oldScene != null) {
+            stage.close();
+        }
+        stage.setScene(newScene);
+        stage.show();
+    }
+
+    public void changeView(String view){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(view + ".fxml"));
+            AnchorPane myAnchorPane = loader.load();
+            anchorPane.getChildren().add(myAnchorPane);
+
+            anchorPane.getChildren().remove(settingsBox);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Quintor/src/main/java/module-info.java
+++ Quintor/src/main/java/module-info.java
@@ -5,6 +5,4 @@ module com.example.quintor {
 
     opens com.example.quintor to javafx.fxml;
     exports com.example.quintor;
-    opens com.example.quintor.controller to javafx.fxml;
-    exports com.example.quintor.controller;
 }
