3f37e8fce630f62efd27a6e30c9e0c9873f831e3
Franca <franca.baars@student.nhlstenden.com>
2023-03-06 13:21:11 +0100

    Added comment and formatter

--- Quintor/src/main/java/com/example/quintor/LoginController.java
+++ Quintor/src/main/java/com/example/quintor/LoginController.java
@@ -25,12 +25,16 @@ public class LoginController {
         return true;
     }
 
+    /**
+     * Checks if the email and password fields are empty, gives error if they are empty.
+     * Sets the email and password, if they can't set gives error.
+     */
     public void pressLoginButton() {
-        if(this.emailField.getText().isEmpty() || this.passwordField.getText().isEmpty()) {
+        if (this.emailField.getText().isEmpty() || this.passwordField.getText().isEmpty()) {
             this.errorLabel.setText("Er is geen e-mailadres of wachtwoord ingevuld");
             return;
         }
-        if(!this.setEmail(this.emailField.getText()) || !this.setHashedPassword(this.passwordField.getText())) {
+        if (!this.setEmail(this.emailField.getText()) || !this.setHashedPassword(this.passwordField.getText())) {
             this.errorLabel.setText("De inloggegevens kloppen niet.");
             return;
         }
