98e63c3aad0a001fe31ed44253611ec38ffab4b1
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2023-03-07 21:18:14 +0100

    added actionevent on button

--- Quintor/src/main/java/com/example/quintor/FileOverviewController.java
+++ Quintor/src/main/java/com/example/quintor/FileOverviewController.java
@@ -1,13 +1,17 @@
 package com.example.quintor;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -34,6 +38,9 @@ public class FileOverviewController implements Initializable {
     @FXML
     private TableView<TranscriptFile> transcriptTable;
 
+    @FXML
+    private Button uploadButton;
+
     /**
      * This method gets called automatically when the contents of the fxml file are fully loaded
      * to perform post-processing on the content
@@ -57,5 +64,16 @@ public class FileOverviewController implements Initializable {
         ObservableList<TranscriptFile> transcriptFiles = transcriptTable.getItems();
         transcriptFiles.add(file);
         transcriptTable.setItems(transcriptFiles);
+    }
+
+    public void uploadButtonAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File uploadedFile = fileChooser.showOpenDialog(null);
+
+        if (uploadedFile == null) {
+            System.out.println("error uploading file");
+        } else {
+            System.out.println("success");
+        }
     }
 }
