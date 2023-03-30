99f28a7c345c6828042107b80be76ccc0bc4868b
Franca <franca.baars@student.nhlstenden.com>
2023-03-30 14:09:01 +0200

    get transactions with xml

--- Quintor/src/main/java/com/example/quintor/getdata/GetCategory.java
+++ Quintor/src/main/java/com/example/quintor/getdata/GetCategory.java
@@ -2,6 +2,7 @@ package com.example.quintor.getdata;
 
 import com.example.quintor.dataobjects.Category;
 import org.json.JSONObject;
+import org.w3c.dom.Element;
 
 public class GetCategory {
     public static Category makeCategoryJSON(JSONObject jsonObject) {
@@ -9,5 +10,11 @@ public class GetCategory {
         String name = (String) jsonObject.get("name");
 
         return new Category(categoryId, name);
+    }
+
+    public static Category makeCategoryXML(Element element) {
+        int id = Integer.parseInt(element.getElementsByTagName("categoryId").item(0).getTextContent());
+        String name = element.getElementsByTagName("name").item(0).getTextContent();
+        return new Category(id, name);
     }
 }
--- Quintor/src/main/java/com/example/quintor/getdata/GetDescription.java
+++ Quintor/src/main/java/com/example/quintor/getdata/GetDescription.java
@@ -2,6 +2,8 @@ package com.example.quintor.getdata;
 
 import com.example.quintor.dataobjects.Description;
 import org.json.JSONObject;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 public class GetDescription {
     public static Description makeDescriptionJSON(JSONObject jsonObject) {
@@ -26,5 +28,33 @@ public class GetDescription {
         return new Description(descriptionId, returnReason, clientReference, endToEndReference, paymentInformationId,
                 instructionId, mandateReference, creditorId, counterpartyId, remittanceInformation, purposeCode,
                 ultimateCreditor, ultimateDebitor, exchangeRate, charges);
+    }
+
+    public static Description makeDescriptionXML(Element element) {
+        int descriptionId = Integer.parseInt(element.getElementsByTagName("descriptionId").item(0).getTextContent());
+        String returnReason = checkElementByTagName(element.getElementsByTagName("returnReason").item(0));
+        String clientReference = checkElementByTagName(element.getElementsByTagName("clientReference").item(0));
+        String endToEndReference = checkElementByTagName(element.getElementsByTagName("endToEndReference").item(0));
+        String paymentInformationId = checkElementByTagName(element.getElementsByTagName("paymentInformationId").item(0));
+        String instructionId = checkElementByTagName(element.getElementsByTagName("instructionId").item(0));
+        String mandateReference = checkElementByTagName(element.getElementsByTagName("mandateReference").item(0));
+        String creditorId = checkElementByTagName(element.getElementsByTagName("creditorId").item(0));
+        String counterpartyId = checkElementByTagName(element.getElementsByTagName("counterpartyId").item(0));
+        String remittanceInformation = checkElementByTagName(element.getElementsByTagName("remittanceInformation").item(0));
+        String purposeCode = checkElementByTagName(element.getElementsByTagName("purposeCode").item(0));
+        String ultimateCreditor = checkElementByTagName(element.getElementsByTagName("ultimateCreditor").item(0));
+        String ultimateDebtor = checkElementByTagName(element.getElementsByTagName("ultimateDebtor").item(0));
+        String exchangeRate = checkElementByTagName(element.getElementsByTagName("exchangeRate").item(0));
+        String charges = checkElementByTagName(element.getElementsByTagName("charges").item(0));
+        return new Description(descriptionId, returnReason, clientReference, endToEndReference, paymentInformationId,
+                instructionId, mandateReference, creditorId, counterpartyId, remittanceInformation, purposeCode, ultimateCreditor,
+                ultimateDebtor, exchangeRate, charges);
+    }
+
+    private static String checkElementByTagName(Node node) {
+        if (node != null) {
+            return node.getTextContent();
+        }
+        return null;
     }
 }
